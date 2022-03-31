22ee6df0eba511852a52196fa7609cd9a9834434
Sandeep Kumar Shukla <sandeepini.2012@gmail.com>
2022-03-31 16:32:55 +0530

    formValidation done

--- src/components/AddUserComponent/AddUserComponent.tsx
+++ src/components/AddUserComponent/AddUserComponent.tsx
@@ -1,48 +1,60 @@
 import axios from "axios";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 
 
-export default function AddUserComponent(props:any) {
-    const [name, setName] = useState("");
-    const [mobile, setMobile] = useState("");
-    //Add User Function
-    function addUser(e: any) {
-        e.preventDefault();
-        // console.log("Name:-",name);
-        // console.log("Mobile:-",mobile);  
-        let reqData: any = {
-            name: name,
-            mobile: mobile
-        }
-        axios.post('http://localhost:3001/api/addUser', reqData).then((res: any) => {
-            console.log(res);
-            formReset();
-        }).catch((err: any) => {
-            console.log(err);
-        });
-        props.addUser();
+export default function AddUserComponent(props: any) {
+    const initialValue = { name: "", mobile: "" };
+    const [formValues, setFormValues] = useState(initialValue);
+    const [formErrors, setFormErrors]:any = useState({});
+    const [isSubmit, setIsSubmit] = useState(false);
+    const handleChange = (event: any) => {
+        const { name, value } = event.target;
+        setFormValues({ ...formValues, [name]: value });
     }
-    //Form Reset
-    function formReset(){
-       setName('');
-       setMobile('');
+    //Add User Function
+    const handleSubmit = (e: any) => {
+        e.preventDefault();
+        setFormErrors(validate(formValues));
+        setIsSubmit(true);
+    }
+    useEffect(() => {
+        if (Object.keys(formErrors).length === 0 && isSubmit) {
+            axios.post('http://localhost:3001/api/addUser', formValues).then((res: any) => {
+                console.log(res);
+            }).catch((err: any) => {
+                console.log(err);
+            });
+            props.addUser();
+        }
+    }, [formErrors])
+    const validate = (values: any) => {
+        const errors: any = {};
+        if (!values.name) {
+            errors.name = "Name is required"
+        }
+        if (!values.mobile) {
+            errors.mobile = "Mobile number is required"
+        }
+        return errors
     }
     return (
         <>
             <div className="container">
                 <div className="p-2 m-2">
-                    <h1>{true ? "Edit user details" : "Add a user"}</h1>
-                    <form onSubmit={(e) => addUser(e)}>
+                    <h1>{false ? "Edit user details" : "Add a user"}</h1>
+                    <form onSubmit={handleSubmit}>
                         <div className="mb-3">
                             <label className="form-label">Name</label>
-                            <input type="text" className="form-control" id="name" name="name" value={name} onChange={(e) => setName(e.target.value)} />
+                            <input type="text" className="form-control" id="name" name="name" value={formValues.name} onChange={handleChange} />
+                            {formErrors?.name ? (<p className="alert alert-danger mt-2">{formErrors?.name}</p>) : null}
                         </div>
                         <div className="mb-3">
                             <label className="form-label">Mobile</label>
-                            <input type="number" className="form-control" id="mobile" name="mobile" value={mobile} onChange={(e) => setMobile(e.target.value)} />
+                            <input type="number" className="form-control" id="mobile" name="mobile" value={formValues.mobile} onChange={handleChange} />
+                            {formErrors?.mobile ? (<p className="alert alert-danger mt-2">{formErrors?.mobile}</p>) : null}
                         </div>
-                        <button type="submit" className="btn btn-primary">{true ? "Update" : "Add"}</button>
+                        <button type="submit" className="btn btn-primary">{false ? "Update" : "Add"}</button>
                     </form>
                 </div>
             </div>
